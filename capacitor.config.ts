5113c539423715acd660712206edf7c3100daec6
AdvaitThePro <advaitthepro@outlook.com>
2022-07-24 15:02:44 +0800

    gospr

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.chargecars1',
+  appName: 'chargecars1',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
